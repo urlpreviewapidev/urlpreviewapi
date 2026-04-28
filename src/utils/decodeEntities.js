88f7e3f0becb6be9536d08ebd7aa3073def1771c
folhetando-dev <folhetando.dev@gmail.com>
2026-04-28 19:56:31 -0300

    fix: adiciona utilitário decodeEntities.js ausente no deploy

---
+++ src/utils/decodeEntities.js
@@ -0,0 +1,13 @@
+// src/utils/decodeEntities.js
+export function decodeHTMLEntities(str) {
+  if (!str) return str;
+  return str
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&apos;/g, "'")
+    .replace(/&#(\d+);/g, (_, dec) => String.fromCharCode(Number(dec)))
+    .replace(/&#x([0-9a-fA-F]+);/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)));
+}
